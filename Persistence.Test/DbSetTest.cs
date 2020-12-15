9b5cd448b1281907a0a44e3c0c4bbe28d296c173
Tim Huang <tim.huang61@gmail.com>
2020-12-16 00:09:59 +0800

    mock dbset but test still failed, because substitute need add iqueryable type.

--- Persistence.Test/DbSetTest.cs
+++ Persistence.Test/DbSetTest.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using NSubstitute;
@@ -16,6 +18,18 @@ namespace Persistence.Test
         {
             _northwindContext           = Substitute.For<NorthwindContext>();
             _northwindContext.Customers = Substitute.For<DbSet<Customer>>();
+            var customers = new List<Customer>
+            {
+                new Customer {CustomerID = 1},
+                new Customer {CustomerID = 2},
+                new Customer {CustomerID = 3},
+            }.AsQueryable();
+
+            var queryable = _northwindContext.Customers.AsQueryable();
+            queryable.Expression.Returns(customers.Expression);
+            queryable.Provider.Returns(customers.Provider);
+            queryable.ElementType.Returns(queryable.ElementType);
+            queryable.GetEnumerator().Returns(new TestEnumerator<Customer>(customers.GetEnumerator()));
         }
 
         [Test]
---
+++ Persistence.Test/TestEnumerator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Persistence.Test
+{
+    public class TestEnumerator<TEntity> : IEnumerator<TEntity>
+    {
+        private readonly IEnumerator<TEntity> _entities;
+
+        public TestEnumerator(IEnumerator<TEntity> entities)
+        {
+            _entities = entities;
+        }
+
+        public bool MoveNext()
+        {
+            return _entities.MoveNext();
+        }
+
+        public void Reset()
+        {
+            _entities.Reset();
+        }
+
+        public TEntity Current => _entities.Current;
+
+        object? IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            _entities.Dispose();
+        }
+    }
+}
